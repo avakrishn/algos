f20f4ae0f0fd93a799d2fdc42fba1249c49ee37c
avakrishn <avakrishn@gmail.com>
2019-05-11 22:32:40 -0700

    add test cases for squares of a sorted array

--- Leet-Code/Squares-Sorted-Array.js
+++ Leet-Code/Squares-Sorted-Array.js
@@ -78,3 +78,21 @@ var sortedSquares = function(A) {
     return result;
     
 };
+
+//-----------Tests
+
+console.log(sortedSquares([-4,-1,0,3,10]));
+console.log('Above Output Should be: [0,1,9,16,100]');
+console.log('--------------------------------------');
+
+console.log(sortedSquares([-7,-3,2,3,11]));
+console.log('Above Output Should be: [4,9,9,49,121]');
+console.log('--------------------------------------');
+
+console.log(sortedSquares([-7,-3]));
+console.log('Above Output Should be: [9,49]');
+console.log('--------------------------------------');
+
+console.log(sortedSquares([2,3,11]));
+console.log('Above Output Should be: [4,9,121]');
+console.log('--------------------------------------');
