a5e99fc98befed41b83936320239e475bf5388cc
avakrishn <avakrishn@gmail.com>
2019-07-27 16:16:32 -0700

    add merge sort algorithm

---
+++ Algorithm-Fundamentals-Data-Structures/ds-template.js
@@ -0,0 +1,69 @@
+'use strict';
+const assert = require('assert');
+
+//------------------------
+
+//------------------------Explanation
+/*
+  -
+*/
+
+//------------------------Constraints/ Edge Cases
+/*
+  - 
+*/
+
+//------------------------Input/ Outputs
+/*
+  ---
+  
+  Input: 
+
+  Output: 
+
+  ---
+
+  Input: 
+
+  Output: 
+
+  ---
+
+*/
+
+//------------------------Approach
+/*
+  -
+*/
+
+//------------------------CODE
+/*
+
+  Time: 
+  Space:
+
+*/
+
+//------------------------Optimization
+/*
+
+  Time: 
+  Space:
+
+*/
+
+//------------------------TESTS:
+
+const runTest = (test, index) => {
+  const error = assert(test, `Test ${index}: Incorrect Result`);
+  if (!error) return `Test ${index}: ${true}`;
+  return error;
+};
+
+const testOne = runTest('Placeholder', 1);
+
+const testArray = [testOne];
+
+for (let test of testArray) {
+  console.log(test);
+}
